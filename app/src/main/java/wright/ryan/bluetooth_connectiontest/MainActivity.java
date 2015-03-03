cb0bf82eac5435ed67037c1553851cc475c23b90
Rwright625 <wright79@students.rowan.edu>
2015-03-03 14:15:45 -0500

    Update 2/24

--- app/src/main/java/wright/ryan/bluetooth_connectiontest/MainActivity.java
+++ app/src/main/java/wright/ryan/bluetooth_connectiontest/MainActivity.java
@@ -3,6 +3,8 @@ package wright.ryan.bluetooth_connectiontest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,14 +12,17 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends Activity {
@@ -33,9 +38,9 @@ public class MainActivity extends Activity {
     private BluetoothDevice remoteDevice;
     public ListView devices;
     public ArrayList<String> deviceNameList = new ArrayList<>();
-    public ArrayAdapter deviceNameAdapter;
-
-
+    public ArrayAdapter<String> deviceNameAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+    private UUID MY_UUID = UUID.fromString("ef637e31-e515-46ff-92d0-d787e45cfdd5");
 
 
     BroadcastReceiver bluetoothState = new BroadcastReceiver() {
@@ -83,7 +88,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupUI();
+
+                setupUI();
+
+        ListeningThread t = new ListeningThread();
+        t.start();
+
+        devices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+                String itemValue = (String) devices.getItemAtPosition(position);
+                String MAC = itemValue.substring(itemValue.length() - 17);
+                BluetoothDevice bluetoothDevice = btAdapter.getRemoteDevice(MAC);
+                // Initiate  a connection request in a seperate thread
+                ConnectThread t = new ConnectThread(bluetoothDevice);
+                t.start();
+
+            }
+        });
+
+
     }/**end onCreate**/
 
 
@@ -102,6 +127,7 @@ public class MainActivity extends Activity {
         /** set display view **/
         disconnect.setVisibility(View.GONE);
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+        pairedDevices = btAdapter.getBondedDevices();
         if(btAdapter.isEnabled()) {
             String address = btAdapter.getAddress();
             String name = btAdapter.getName();
@@ -168,15 +194,16 @@ public class MainActivity extends Activity {
         String lastUsedRemoteDevice = getLastUsedRemoteBTDevice();
         if (lastUsedRemoteDevice != null) {
             toastText = "Checking for known paired devices, namely: " + lastUsedRemoteDevice;
-            Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
             /** see if this device is in a list of current visible (?), paired devices **/
             Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
             for (BluetoothDevice pairedDevice : pairedDevices) {
-//                if (pairedDevice.getAddress().equals(lastUsedRemoteDevice)) {
-//                    toastText = "Found device: " + pairedDevice.getName() + "@" + lastUsedRemoteDevice;
-//                    Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
-//                    remoteDevice = pairedDevice;
-//                }
+                if (pairedDevice.getAddress().equals(lastUsedRemoteDevice)) {
+                    toastText = "Found device: " + pairedDevice.getName() + "@" + lastUsedRemoteDevice;
+                    Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+                    remoteDevice = pairedDevice;
+                }
             }
         } /**end if **/
         if (remoteDevice == null) {
@@ -205,14 +232,15 @@ public class MainActivity extends Activity {
 
 
             remoteDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            String pairDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE);
 
             String remoteDeviceName = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
             if (remoteDeviceName == null){
                 remoteDeviceName = remoteDevice.getAddress();
             }
 
-            toastText = "Discovered: " + remoteDeviceName;
-            Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+            //toastText = "Discovered: " + remoteDeviceName;
+            //Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
             deviceNameList.add(remoteDeviceName);
             deviceNameAdapter.notifyDataSetChanged();
             /** statusUpdate.setText(statusText);**/
@@ -227,10 +255,110 @@ public class MainActivity extends Activity {
         return result;
     }
 
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) { }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            btAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) { }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+//            manageConnectedSocket(mmSocket);
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private class ListeningThread extends Thread {
+        private final BluetoothServerSocket bluetoothServerSocket;
+
+        public ListeningThread() {
+            BluetoothServerSocket temp = null;
+            try {
+                temp = btAdapter.listenUsingRfcommWithServiceRecord(getString(R.string.app_name), MY_UUID);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bluetoothServerSocket = temp;
+        }
+
+        public void run() {
+            BluetoothSocket bluetoothSocket;
+            // This will block while listening until a BluetoothSocket is returned
+            // or an exception occurs
+            while (true) {
+                try {
+                    bluetoothSocket = bluetoothServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection is accepted
+                if (bluetoothSocket != null) {
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "A connection has been accepted.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // Manage the connection in a separate thread
+
+                    try {
+                        bluetoothServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Cancel the listening socket and terminate the thread
+        public void cancel() {
+            try {
+                bluetoothServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 
 }
 
 /** End MainActivity **/
-
-
